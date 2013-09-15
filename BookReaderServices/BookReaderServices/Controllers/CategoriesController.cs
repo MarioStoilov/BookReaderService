244d3aeec2dafa276fa4df496bf9432f548ef6e0
naturalna <naturalna89@gmail.com>
2013-09-15 21:11:43 +0300

    categoriesController

---
+++ BookReaderServices/BookReaderServices/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using BookReaderServices.Attributes;
+using BookReaderServices.Models;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ValueProviders;
+
+namespace BookReaderServices.Controllers
+{
+    public class CategoriesController : BaseApiController
+    {
+        // GET api/categories/getallcategories
+        [ActionName("getallcategories")]
+        public HttpResponseMessage Get([ValueProvider(typeof(HeaderValueProviderFactory<string>))] string sessionKey)
+        {
+            var responseMsg = this.PerformOperationAndHandleExceptions(
+                () =>
+                {
+                    var context = new BookReaderEntities();
+                    using (context)
+                    {
+                        var user = context.Users.FirstOrDefault(
+                                usr => usr.sessionKey == sessionKey);
+
+                        if (user == null)
+                        {
+                            throw new InvalidOperationException("Users does not exist");
+                        }
+
+                        var allCategories =
+                            from cat in context.Categories
+                            select new CategoriesDTO()
+                            {
+                                Title = cat.Title,
+                            };
+                        var response =
+                            this.Request.CreateResponse(HttpStatusCode.OK, allCategories.ToList()); //very important to call .ToList() or else it fails to serialize
+                        return response;
+                    }
+                });
+
+            return responseMsg;
+        }
+    }
+}
---
+++ BookReaderServices/BookReaderServices/Models/CategoriesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace BookReaderServices.Models
+{
+
+        [DataContract]
+        public class CategoriesDTO
+        {
+            [DataMember(Name = "title")]
+            public string Title { get; set; }
+        }
+    
+}
